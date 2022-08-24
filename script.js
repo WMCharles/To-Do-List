696122aa1cb487456995a21d40d0180765529089
WMCharles <charles.wafula@student.moringaschool.com>
2022-08-24 19:11:45 +0300

    added check & delete btn

--- script.js
+++ script.js
@@ -7,9 +7,17 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   })
   
+//   function that adds the 
   function handleTask(todo){
     let p = document.createElement('p')
-    p.innerText = todo
+    let t = document.createTextNode(todo)
+    let x = document.createElement('span')
+    let y = document.createElement('span')
+    y.innerHTML = '<input type="submit" value="done">'
+    x.innerHTML = '<input type="submit" value="x">'
+    p.appendChild(t)
+    p.appendChild(y)
+    p.appendChild(x)
     document.getElementById('content').appendChild(p)
     document.getElementById('new_task_description').value = "";
   }
